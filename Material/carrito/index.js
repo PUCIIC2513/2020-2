10dd02d5df668a4788c6fc756016d6580c9a05a9
B-Dominguez <bidomunix@DESKTOP-GRFF0IJ.localdomain>
2020-10-15 16:10:46 -0300

    Update hello world with koa-router

--- Material/carrito/index.js
+++ Material/carrito/index.js
@@ -1,10 +1,16 @@
 'use strict';
 
 const koa = require('koa')
+const koaRouter = require('koa-router')
+
 const app = new koa()
+const router = new koaRouter()
 
-app.use(function *(){
-  this.body = "Hello World !!!";
-});
+router.get('koala', '/', (ctx) => {
+    ctx.body = "Welcome! To the Koala Book of Everything!"
+})
 
-app.listen(3000)
+app.use(router.routes())
+    .use(router.allowedMethods())
+  
+app.listen(3000, () => console.log('running on port 3000'))
