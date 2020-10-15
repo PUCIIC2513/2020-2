251b8edde12b8d9e82045c7919f152bb14d13a54
B-Dominguez <bidomunix@DESKTOP-GRFF0IJ.localdomain>
2020-10-15 16:52:37 -0300

    Update index.js Hello World with koa-session count example

--- Material/carrito/index.js
+++ Material/carrito/index.js
@@ -1,13 +1,24 @@
 'use strict';
 
+var session = require('koa-session');
 const koa = require('koa')
 const koaRouter = require('koa-router')
 
 const app = new koa()
 const router = new koaRouter()
 
+app.keys = ['Shh, its a secret!'];
+app.use(session(app));  // Include the session middleware
+
 router.get('koala', '/', (ctx) => {
     ctx.body = "Welcome! To the Koala Book of Everything!"
+    var n = ctx.session.views || 0;
+    ctx.session.views = ++n;
+    
+    if(n === 1)
+       ctx.body = 'Welcome here for the first time!';
+    else
+       ctx.body = "You've visited this page " + n + " times!";
 })
 
 app.use(router.routes())
