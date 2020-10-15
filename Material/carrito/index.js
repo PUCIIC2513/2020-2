d585dd4d987c17654b576d1cdc201b20ff7f24b0
B-Dominguez <bidomunix@DESKTOP-GRFF0IJ.localdomain>
2020-10-15 17:00:44 -0300

    Add more usage examples

--- Material/carrito/index.js
+++ Material/carrito/index.js
@@ -10,10 +10,34 @@ const router = new koaRouter()
 app.keys = ['Shh, its a secret!'];
 app.use(session(app));  // Include the session middleware
 
+
+// ADVERTENCIA WARNING: Fuera de este ejemplo, routers deberían ser llamados desde otro archivo
 router.get('koala', '/', (ctx) => {
     ctx.body = "Welcome! To the Koala Book of Everything!"
     var n = ctx.session.views || 0;
     ctx.session.views = ++n;
+    
+    if(n === 1)
+       ctx.body = 'Welcome here for the first time!';
+    else
+       ctx.body = "You've visited this page " + n + " times!";
+})
+
+router.get('koala', '/count', (ctx) => {
+    ctx.body = "Welcome! To the Koala Book of Everything!"
+    var n = ctx.session.views || 0;
+    ctx.session.views = ++n;
+    
+    if(n === 1)
+       ctx.body = 'Welcome here for the first time!';
+    else
+       ctx.body = "You've visited this page " + n + " times!";
+})
+
+router.get('koala', '/count2', (ctx) => {
+    ctx.body = "Welcome! To the Koala Book of Everything!"
+    var n = ctx.session.views2 || 0;
+    ctx.session.views2 = ++n;
     
     if(n === 1)
        ctx.body = 'Welcome here for the first time!';
