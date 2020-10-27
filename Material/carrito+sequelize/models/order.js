43ef9b285f8d2b8932d5269f6935bd9685967be8
Cschavez <cschavez.uc.cl>
2020-10-27 13:52:13 -0300

    associations in orders

--- Material/carrito+sequelize/models/order.js
+++ Material/carrito+sequelize/models/order.js
@@ -10,7 +10,8 @@ module.exports = (sequelize, DataTypes) => {
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      // define association here
+      models.order.belongsTo(models.costumer);
+      models.order.hasMany(models.product);
     }
   };
   order.init({
