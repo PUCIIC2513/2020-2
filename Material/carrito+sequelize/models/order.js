e8563485ab675583e83a16f5ab934cfa154677c0
Cschavez <cschavez.uc.cl>
2020-10-27 13:53:41 -0300

    correct model association in orders

--- Material/carrito+sequelize/models/order.js
+++ Material/carrito+sequelize/models/order.js
@@ -10,7 +10,7 @@ module.exports = (sequelize, DataTypes) => {
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      models.order.belongsTo(models.costumer);
+      models.order.belongsTo(models.customer);
       models.order.hasMany(models.product);
     }
   };
