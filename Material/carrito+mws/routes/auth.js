d4bc4ce96ff940d51af8d2521b96249fb64b0aea
jorge <jabecerra@uc.cl>
2020-11-01 15:19:45 -0300

    refactor: use auth midleware in logout route

--- Material/carrito+mws/routes/auth.js
+++ Material/carrito+mws/routes/auth.js
@@ -2,6 +2,8 @@ const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 const KoaRouter = require('koa-router');
 
+const customerAuth = require('../middlewares/auth');
+
 const router = new KoaRouter();
 
 const TOKEN_SECRET = 'este es un secreto, no se lo cuentes a nadie!!';
@@ -32,29 +34,11 @@ router.post('login', '/login', async (ctx) => {
   }
 });
 
-router.post('logout', '/logout', async (ctx) => {
-  const { body } = ctx.request;
-  const { token } = body;
+router.delete('logout', '/logout', customerAuth, async (ctx) => {
+  const { currentCustomer } = ctx;
 
-  try {
-    const decoded = jwt.verify(token, TOKEN_SECRET);
-    const { email } = decoded;
-
-    const customer = await ctx.db.customer.findOne({
-      where: {
-        email,
-      },
-    });
-
-    if (customer) {
-      await customer.update({ token: null });
-      ctx.body = { msg: 'Sesión cerrada correctamente' };
-    } else {
-      ctx.body = { error: 'Crendenciales inválidas' };
-    }
-  } catch (error) {
-    return (ctx.body = { error: 'Crendenciales inválidas' });
-  }
+  currentCustomer.update({ token: null });
+  ctx.body = { msg: 'Sesión cerrada correctamente' };
 });
 
 module.exports = router;
