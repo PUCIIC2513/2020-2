98da747896065b6baee7cb99308971c18f78578a
jorge <jabecerra@uc.cl>
2020-11-01 12:02:10 -0300

    fix: typo in customer model

--- Material/carrito+mws/models/customer.js
+++ Material/carrito+mws/models/customer.js
@@ -8,7 +8,7 @@ module.exports = (sequelize, DataTypes) => {
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      models.customer.hasMany(models.orders);
+      models.customer.hasMany(models.order);
     }
   }
   customer.init(
