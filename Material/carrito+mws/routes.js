a30ff1d8a420a29eb0f40fede419765e8b9442a1
jorge <jabecerra@uc.cl>
2020-11-01 15:18:27 -0300

    feat: get products route

--- Material/carrito+mws/routes.js
+++ Material/carrito+mws/routes.js
@@ -2,12 +2,14 @@ const KoaRouter = require('koa-router');
 
 const auth = require('./routes/auth');
 const customers = require('./routes/customers');
+const products = require('./routes/products');
 const orders = require('./routes/orders');
 
 const router = new KoaRouter();
 
 router.use('/auth', auth.routes());
 router.use('/customers', customers.routes());
+router.use('/products', products.routes());
 router.use('/orders', orders.routes());
 
 module.exports = router;
