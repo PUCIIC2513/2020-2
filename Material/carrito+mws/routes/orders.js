6d2d5755215a1f86540063640f1f41f4217980ae
jorge <jabecerra@uc.cl>
2020-11-01 15:22:45 -0300

    feat: add products to orders and use  orders middlewares

--- Material/carrito+mws/routes/orders.js
+++ Material/carrito+mws/routes/orders.js
@@ -1,26 +1,56 @@
 const KoaRouter = require('koa-router');
+
+const customerAuth = require('../middlewares/auth');
+const { orderGuard, validateStock } = require('../middlewares/orders');
+
 const router = new KoaRouter();
 
-router.get('orders', '/:id', async (ctx) => {
-  const order = await ctx.db.order.findByPk(ctx.params.id);
+router.get('orders', '/:id', customerAuth, orderGuard, async (ctx) => {
+  const { currentOrder } = ctx;
+
+  ctx.body = currentOrder;
+});
+
+router.post('orders.new', '/new', customerAuth, async (ctx) => {
+  const { currentCustomer } = ctx;
+  const { body } = ctx.request;
+
+  const order = await currentCustomer.createOrder(body);
+
   ctx.body = order;
-})
+});
 
-router.post('orders.new', '/new', async (ctx) => {
-   const body = await ctx.request.body;
-   const new_order = await ctx.db.order.create(body);
-   ctx.body = new_order;
-})
+router.post(
+  'orders.add',
+  '/:id/add',
+  customerAuth,
+  orderGuard,
+  validateStock,
+  async (ctx) => {
+    const { currentOrder } = ctx;
+    const { products } = ctx.request.body;
 
-router.patch('orders.update', '/:id', async (ctx) => {
-  const body = await ctx.request.body;
-  const order = await ctx.db.order.findByPk(ctx.params.id);
-  await order.update(body);
-})
+    products.map(async (productId) => {
+      const product = await ctx.db.product.findByPk(productId);
+      await product.update({ stock: product.stock - 1 });
+      await currentOrder.addProduct(product);
+    });
 
-router.del('orders.delete', '/:id', async (ctx) => {
-  const order = await ctx.db.order.findByPk(ctx.params.id);
-  await order.destroy();
-})
+    const orderProducts = await currentOrder.getProducts();
+    ctx.body = { order: currentOrder, products: orderProducts };
+  }
+);
+
+router.patch('orders.update', '/:id', customerAuth, orderGuard, async (ctx) => {
+  const { currentOrder } = ctx;
+  await currentOrder.update(body);
+  ctx.body = currentOrder;
+});
+
+router.del('orders.delete', '/:id', customerAuth, orderGuard, async (ctx) => {
+  const { currentOrder } = ctx;
+  await currentOrder.destroy();
+  ctx.body = currentOrder;
+});
 
 module.exports = router;
